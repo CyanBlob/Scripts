586fcaa103ee67aa1ad8a0d488cdc545e679f858
Andrew <Andrewt1993@yahoo.com>
2014-01-10 00:24:23 +0000

    Update downloadSort.py

--- downloadSort.py
+++ downloadSort.py
@@ -2,35 +2,36 @@
 import shutil
 import os
 import re
-source = os.listdir("/home/andrew-fedora/Downloads")
-destination = "/home/andrew-fedora/Documents"
+source = os.listdir("/home/andrew-arch/Downloads")
+destination = "/home/andrew-arch/Documents"
 test = ""
 for files in source:
     if files.endswith(".txt") or files.endswith(".pdf") or files.endswith(".doc") or files.endswith(".docx") or files.endswith(".odt") or files.endswith(".xlsx"):
-            print files
-            destination = "/home/andrew-fedora/Documents"
-            test = "/home/andrew-fedora/Downloads/" + files
+            destination = "/home/andrew-arch/Documents/Downloaded"
+            test = "/home/andrew-arch/Downloads/" + files
             shutil.copy(test,destination)
             os.remove(test)
     
     elif files.endswith(".mp3") or files.endswith(".m4a") or files.endswith(".wav") or files.endswith(".pdf"):
-            print files
-            destination = "/home/andrew-fedora/Music"
-            test = "/home/andrew-fedora/Downloads/" + files
+            destination = "/home/andrew-arch/Music/Downloaded"
+            test = "/home/andrew-arch/Downloads/" + files
             shutil.copy(test,destination)
             os.remove(test)
 
     elif files.endswith(".avi") or files.endswith(".mpg") or files.endswith(".mkv") or files.endswith(".divx") or files.endswith(".vob") or files.endswith(".mp4") or files.endswith(".m4v"):
-            print files
-            destination = "/home/andrew-fedora/Videos"
-            test = "/home/andrew-fedora/Downloads/" + files
+            destination = "/home/andrew-arch/Videos/Downloaded"
+            test = "/home/andrew-arch/Downloads/" + files
             shutil.copy(test,destination)
             os.remove(test)
     
     elif files.endswith(".jpg") or files.endswith(".jpeg") or files.endswith(".png") or files.endswith(".raw") or files.endswith(".gif") or files.endswith(".bmp"):
-            print files
-            destination = "/home/andrew-fedora/Pictures"
-            test = "/home/andrew-fedora/Downloads/" + files
+            destination = "/home/andrew-arch/Pictures/Downloaded"
+            test = "/home/andrew-arch/Downloads/" + files
             shutil.copy(test,destination)
             os.remove(test)
-    
+       
+    elif files.endswith(".torrent") or files.endswith(".iso"):
+            destination = "/home/andrew-arch/Downloads/Torrents"
+            test = "/home/andrew-arch/Downloads/" + files
+            shutil.copy(test,destination)
+            os.remove(test)
